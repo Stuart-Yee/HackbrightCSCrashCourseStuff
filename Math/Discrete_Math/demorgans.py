3d99629bd2fec2beb8e9ba32df2641c377bd9b43
Stuart-Yee <ki6fzb@gmail.com>
2024-08-20 21:57:00 -0700

    lots of things

---
+++ Math/Discrete_Math/demorgans.py
@@ -0,0 +1,9 @@
+
+
+class friend:
+    def __init__(self):
+        self.is_taylor_swift_fan = True
+        self.is_beyonce_fan = True
+        self.is_lady_gag_fan = True
+
+        self.needs_playlist = not self.is_taylor_swift_fan
---
+++ Math/Functions/functions.py
@@ -0,0 +1,4 @@
+def my_function(my_num):
+    return my_num ** 2
+
+print(my_function(4))
